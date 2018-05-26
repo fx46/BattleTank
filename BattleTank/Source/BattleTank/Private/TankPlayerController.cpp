769508af9e8e28f74435ba4ea4d8c167a9ae4ac0
FX Legault <31199346+fx46@users.noreply.github.com>
2018-05-25 23:16:19 -0400

    Beginning of classes Tank and TankPlayerController, also logging in console to verify wich tank is possesed by the player

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tankou"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing: %s"), *(ControlledTank->GetName()));
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+}
